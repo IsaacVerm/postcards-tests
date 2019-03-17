2efdeaa344aa0c35179cd125f45b5a70326fbdb7
IsaacVerm <isaacverm@gmail.com>
2019-03-17 19:23:47 +0100

    Fix broken Cypress test example

--- cypress/integration/example_spec.js
+++ cypress/integration/example_spec.js
@@ -1,6 +1,6 @@
 describe('hello', function() {
   it('gives me my personalized credentials', function() {
-    cy.visit('https://hello-flask-example.herokuapp.com/credentials/view/Isaac');
+    cy.visit(`${Cypress.env('baseUrl')}/credentials/view/Isaac`);
     cy.get('body > p')
       .should('contain', 'credentials')
       .should('contain', 'Isaac');
